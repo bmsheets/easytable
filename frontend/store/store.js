901b3769a4b14aec1d968f659207f8f809aa34fd
Brandt <brandtsheets@gmail.com>
2017-11-21 13:51:12 -0800

    Add empty app component files and import redux-logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,5 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers/root';
 
-export default () => createStore(rootReducer, applyMiddleware(thunk));
+export default (preloadedState = {}) => (
+  createStore(rootReducer,
+              preloadedState,
+              applyMiddleware(createLogger(), thunk)
+));
