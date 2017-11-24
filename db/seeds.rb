fc9a945fb25939c78fd0a69a48c15820c1f1ccf5
Brandt <brandtsheets@gmail.com>
2017-11-24 09:43:16 -0800

    Add some users to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create({first_name: "Alice", email: "alice@mail.com", password: "password"})
+User.create({first_name: "Bob", email: "bob@mail.com", password: "password"})
+User.create({first_name: "Guest", email: "guest@mail.com", password: "password"})
