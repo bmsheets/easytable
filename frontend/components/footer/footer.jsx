faf83d9feba4971d87f8f55fdc68e3853a498ea6
Brandt <brandtsheets@gmail.com>
2018-01-15 14:11:59 -0800

    Add icons and links to footer. Revise footer styling

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,25 +5,45 @@ export default () => {
     <div className="footer">
       <div className="footer-box-container">
         <div className="footer-box-1">
-          <span>Description</span>
-          <p>Welcome to EasyTable - Your destination for restaurant bliss.
+          <span>About</span>
+          <p className="about-paragraph">Welcome to EasyTable - Your destination for restaurant bliss.
           This web application was built using Ruby on Rails and React/Redux
           as a tribute to the popular reservation service OpenTable.
         </p>
         </div>
         <div className="footer-box-2">
           <span>Links</span>
-          <ul>
-            <li>Github</li>
-            <li>LinkedIn</li>
-            <li>README</li>
-            <li>Email</li>
+          <ul className="fa-ul footer-links">
+            <li>
+              <a href="http://github.com/bmsheets">
+                <i class="fa fa-github fa-li" aria-hidden="true"></i>
+                Github
+              </a>
+            </li>
+            <li>
+              <a href="http://linkedin.com/in/bmsheets">
+                <i class="fa fa-linkedin-square fa-li" aria-hidden="true"></i>
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a href="http://github.com/bmsheets/easytable">
+                <i class="fa fa-book fa-li" aria-hidden="true"></i>
+                README
+              </a>
+            </li>
+            <li>
+              <a href="mailto:brandtsheets@gmail.com">
+                <i class="fa fa-envelope-o fa-li" aria-hidden="true"></i>
+                Email
+              </a>
+            </li>
           </ul>
         </div>
       </div>
       <div className="footer-bar">
         <span className="footer-banner">
-          Brandt Sheets 2018 All Rights Reserved
+          Built by Brandt Sheets, 2017-2018
         </span>
       </div>
     </div>
