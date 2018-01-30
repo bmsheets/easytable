1346413d107bda5c10de7acee125c2ba1ad564e4
Brandt <brandtsheets@gmail.com>
2018-01-29 17:56:49 -0800

    Add featured_areas component and styles including image urls

---
+++ frontend/components/main/featured_areas.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div className="featured-areas">
+      <h3>Featured Areas</h3>
+      <div className="location-boxes">
+        <div className="location-box-1"></div>
+        <div className="location-box-2"></div>
+        <div className="location-box-3"></div>
+        <div className="location-box-4"></div>
+        <div className="location-box-5"></div>
+        <div className="location-box-6"></div>
+      </div>
+      <div className="location-links">
+        <div className="location-col-1"></div>
+        <div className="location-col-2"></div>
+        <div className="location-col-3"></div>
+        <div className="location-col-4"></div>
+      </div>
+   </div>
+  );
+};
--- frontend/components/main/main_page.jsx
+++ frontend/components/main/main_page.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import RestaurantFormContainer from '../restaurants/restaurant_form_container';
 import RestaurantIndexContainer from '../restaurants/restaurant_index_container';
 import SearchBarContainer from './search_bar_container';
+import FeaturedAreas from './featured_areas';
 
 export default () => {
   return (
     <div className="main-page">
       <div className="banner-image"/>
       <SearchBarContainer />
+      <FeaturedAreas />
    </div>
   );
 };
